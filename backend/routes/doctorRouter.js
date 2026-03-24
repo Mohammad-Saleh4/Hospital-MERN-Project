868db0d748e42ddcd8212a30b5b1f8fd7f2d7669
Mohammad Saleh <mohammad.saleh.3004@gmail.com>
2026-03-24 17:11:38 +0200

    add doctorRouter with CRUD operations and image upload handling

---
+++ backend/routes/doctorRouter.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import multer from 'multer';
+import { createDoctor, deleteDoctor, doctorLogin, getDoctorById, getDoctors, toggleAvailability, updateDoctor } from '../controllers/doctorController';
+
+import doctorAuth from '../middlewares/doctorAuth';
+
+const upload=multer({
+dest:"/tmp"
+});
+
+const doctorRouter=express.Router();
+
+doctorRouter.get("/",getDoctors);
+doctorRouter.post("/login",doctorLogin);
+doctorRouter.get("/:id",getDoctorById);
+doctorRouter.post("/",upload.single("image"),createDoctor);
+
+
+//afterlogin
+
+doctorRouter.put("/:id",doctorAuth,upload.single("image"),updateDoctor);
+doctorRouter.post("/:id/toggle-availability",doctorAuth,toggleAvailability);
+doctorRouter.delete("/:id",deleteDoctor);
+
+export default doctorRouter;
